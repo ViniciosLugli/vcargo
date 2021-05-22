342bd8853236a7ecedf0d4453844d5b8a13062d1
Vinicios Lugli <vinicioslugli@gmail.com>
2021-05-21 21:11:38 -0300

    Change default for unwrap

--- src/custom_commands/mod.rs
+++ src/custom_commands/mod.rs
@@ -2,8 +2,7 @@ use fs_extra::dir::{copy, CopyOptions};
 use std::env;
 
 pub fn copy_folder(to: &str) -> bool {
-    let _default_path: String = env::var("_DEFAULT_PATH_")
-        .unwrap_or("C:/Users/vinic/Documents/codigos/rust/_DEFAULT_".to_string());
+    let _default_path: String = env::var("_DEFAULT_PATH_").unwrap_or("./_DEFAULT_".to_string());
 
     let options = CopyOptions::new();
 
